415c0dfa2a5a1ca85841f0f0086dd959c85e0fd9
hoorain-sundas <110491795+hoorain-sundas@users.noreply.github.com>
2022-09-03 01:41:19 -0700

    JS Assignment

---
+++ app.js
@@ -0,0 +1,337 @@
+// ------Chapter No.  01---------
+//Q No. 1
+// alert("Hello! Welcome Everyone.");
+
+//Q No. 2
+ // alert("Error! Please enter a valid password.");
+
+ //Q No. 3
+ // alert("Welcome to JS Land...\n Happy Coding!");
+
+ //Q No. 4 (a)
+ // alert("Welcome to JS Land...");
+
+ //Q No. 4 (b)
+ // alert("Happy Coding!\n [] Prevent this page from creating additional dialogs.");
+
+ //Q No. 5
+ // console.log("Hello.. I can run JS through my web browser's console.");
+
+ // ------Chapter No.  02---------
+//Q No. 1
+// var Username;
+
+//Q No. 2
+// var myName = "Hoorain-Sundas Qureshi";
+
+//Q No. 3
+// var message;
+// message = "Hello World";
+// alert(message);
+
+//Q No. 4
+// alert(" \n                                                Jhone Doe \n ");
+// alert(" \n                                              15 years old \n ");
+// alert(" \n                           Certified Mobile Application Development \n ");
+
+//Q No. 5
+//alert(" \n                                             PIZZA \n                                             PIZZ\n                                             PIZ\n                                             PI\n                                             P");
+
+//Q No. 6
+// var email = "hoorain160@gmail.com";
+// var A = " My email address is ";
+// alert(A + email);
+
+//Q No. 7
+// var A = "I am trying to learn from the Book ";
+// var book = "A smarter way to learn JavaScript";
+// alert(A + book);
+
+//Q No. 8
+//document.write("Yah! I can write HTML content through JavaScript");
+
+//Q No. 9
+//alert("\n    ▬▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬    ");
+
+ // ------Chapter No.  03---------
+ //Q No. 1
+   // var age = 30;
+   // var A = " I am ";
+   // var B = " years old ";
+    // alert(" I am " + age + " years old ");
+  
+ //Q No. 2
+// var a = 1;
+// if(a >= 1){
+// alert("you have visited this site " + a + " times");
+// a++;}
+
+//Q No. 3
+//var birthYear = 1992;
+//document.write("My birth year is ", birthYear, "<br>" , "Data type of my declared variable is number")
+
+//Q No. 4
+// var VN = "<b>John Doe</b> ";
+// var PT = "<b>T-shirt(s)</b> ";
+// var Quantity = "<b>5</b> ";
+// document.write(VN + " ordered " + Quantity + PT, "on XYZ Clothing store");
+
+
+ // ------Chapter No.  04---------
+ //Q No. 1
+ // var a , b , c;
+
+ //Q No. 2
+ // --- Legal variables:
+
+ // var userAlert;
+ // var myVar;
+ // var $ab;
+ // var a_bc;
+ // var ali_1;
+
+ // --- Illegal variables:
+
+ // var a-bc;
+ // var 1ali;
+ // var 123;
+ // var &umer;
+ // var abc.;
+
+ // Q No. 3
+//  var a = "<b>Rules for naming JS variables</b>";
+//  var b = "Variable names can only contain letters, numbers, $ and _. For example $my_1stVariable";
+//  var c = "Variables must begin with a letter, $ or _. For example $name, _name or name";
+//  var d = "Variable names are case sensitive ";
+//  var e = "Variable names should not be JS keywords ";
+//  document.write(a , "<br>" , "<br>" , "<br>" , b , "<br>" , c , "<br>" , d , "<br>" , e );
+
+// ------Chapter No.  05---------
+
+// Q No. 1
+// var a = 3;
+// var b = 5;
+// var c = a + b;
+// document.write("Sum of ", a , " and " , b , " is " , c);
+
+// Q No. 2
+// var a = 3;
+// var b = 5;
+// var c = b - a;
+// document.write("Subtraction of ", a , " from " , b , " is " , c , "<br>");
+// var c = a * b;
+// document.write("Multiplication of ", a , " and " , b , " is " , c , "<br>");
+// var c = a / b;
+// document.write("Division of ", a , " and " , b , " is " , c , "<br>");
+// var c = a % b;
+// document.write("modulus of ", a , " and " , b , " is " , c);
+
+// Q No. 3
+// var a;
+// document.write("Value after variable declaration is " , a , "<br>");
+// a = 5;
+// document.write("Initial value: " , a , "<br>");
+// a++;
+// document.write("Value after increament is: " , a , "<br>");
+// a = a + 7;
+// document.write("Value after addition is: " , a , "<br>");
+// a--;
+// document.write("Value after decrement is: " , a , "<br>");
+// a = a % 3;
+// document.write("The remainder is: " , a);
+
+// Q No. 4
+// var a = 600;
+// var b = a * 5;
+// document.write("Total cost to buy 5 tickets to a movie is " , b , "PKR")
+
+// Q No. 5
+// var a = 1;
+// var b = 4;
+// document.write("Table of 4 <br>");
+//     var c = b * a;
+//     document.write(b , "x" , a , "=" , c , "<br>");
+//     a++;
+//     var c = b * a;
+//     document.write(b , "x" , a , "=" , c , "<br>");
+//     a++;
+//     var c = b * a;
+//     document.write(b , "x" , a , "=" , c , "<br>");
+//     a++;
+//     var c = b * a;
+//     document.write(b , "x" , a , "=" , c , "<br>");
+//     a++;
+//     var c = b * a;
+//     document.write(b , "x" , a , "=" , c , "<br>");
+//     a++;
+//     var c = b * a;
+//     document.write(b , "x" , a , "=" , c , "<br>");
+//     a++;
+//     var c = b * a;
+//     document.write(b , "x" , a , "=" , c , "<br>");
+//     a++;
+//     var c = b * a;
+//     document.write(b , "x" , a , "=" , c , "<br>");
+//     a++;
+//     var c = b * a;
+//     document.write(b , "x" , a , "=" , c , "<br>");
+//     a++;
+//     var c = b * a;
+//     document.write(b , "x" , a , "=" , c , "<br>");
+    
+
+// Q No. 6
+// var C = 25;
+// var F_T = (C * 9/5) + 32;
+// document.write(C , "oC is " , F_T , "oF <br>");
+// var F = 70;
+// var C_T = (F-32) * 5/9;
+// document.write(F , "oF is " , C_T , "oC");
+
+// Q No. 7
+// var item_1 = 650;
+// var item_2 = 100;
+// var q_1 = 3;
+// var q_2 = 7;
+// var Ship_charges = 100;
+// document.write("<b>Shopping Cart</b> <br><br><br><br>");
+// document.write("Price of item 1 is " , item_1 , "<br>");
+// document.write("Quantity of item 1 is " , q_1 , "<br>");
+// document.write("Price of item 2 is " , item_2 , "<br>");
+// document.write("Quantity of item 2 is " , q_2 , "<br>");
+// document.write("Shipping charges " , Ship_charges , "<br> <br>");
+// var T_C = (item_1 * q_1) + (item_2 * q_2) + Ship_charges;
+// document.write("Total cost of your order is " , T_C);
+
+// Q No. 8
+
+// var total_marks = 980; 
+// var obtain_marks = 804;
+// var percentage = (obtain_marks * 100) / total_marks;
+// document.write("<b> Marks Sheet </b> <br><br><br>" , "Total marks: " , total_marks , "<br>" , "Marks obtained: " , obtain_marks , "<br>" , "Percentage: " , percentage , "%" )
+
+// Q No. 9
+// var US_dollar = 10;
+// var Saudi_Riyal = 25;
+// var Total_PKR = (US_dollar * 104.80) + (Saudi_Riyal * 28);
+// document.write("<br><b> Currency in PKR </b> <br><br><br>" , "Total Currency in PKR: " , Total_PKR);
+
+// Q No. 10
+// var a = 2;
+// var b = ((a + 5) * 10) / 2;
+
+// Q No. 11
+// var C_year = 2022;
+// var B_year = 1992;
+// var a = C_year - B_year;
+// document.write("<br><b> Age Calculator </b><br><br><br>", "Current year: " , C_year , "<br>" , "Birth year: " , B_year , "<br>" , "Your age is: " , a);
+
+// Q No. 12
+// var r = 20;
+// var c = 2 * 3.142 * r;
+// var a = 3.142 * r * r;
+// document.write("<br><b> The Geometrizer </b><br><br><br>" , "Radius of a circle: " , r , "<br>The circumference is " , c , " <br>The area is: " , a );
+
+// Q No. 13
+// var F_snack = "Chocolate chip";
+// var age = 15;
+// var m_age = 65;
+// var A_snacks = 3; // per day
+// var total = (m_age - age) * A_snacks;
+// document.write("<br><b> The Lifetime Supply Calculator </b><br><br><br>" , "Favourite Snack: " , F_snack , "<br>Current Age: " , age , "<br>Estimated Maximum Age: " , m_age , "<br>Amount of snacks per day: " , A_snacks , "<br>You will need " , total , " to last you until the ripe old age of " , m_age );
+
+// ------Chapter No.  06 to 09---------
+// Q No. 1
+// var a = 10;
+// document.write("Result <br> The value of a is: " , a , "<br><br> .........................................." , "<br><br>The value of ++a is: ", ++a , "<br>Now the value of a is: " , a ,
+//"<br><br>The value of a++ is: ", a++ , "<br>Now the value of a is: " , a , "<br><br>The value of --a is: ", --a , "<br>Now the value of a is: " , a , "<br><br>The value of a-- is: ", a-- , "<br>Now the value of a is: " , a  );
+
+// Q No. 2
+// var a = 2 , b = 1;
+// document.write("a is " , a , "<br>b is " , b );
+// var result = --a - --b + ++b + b--;
+//Explanation:
+// --a is 1 = 1
+// --a - --b is 1 - 0 = 1 
+// --a - --b + ++b is 1 - 0 + 1 = 2
+// --a - --b + ++b + b-- is 1 - 0 + 1 + 1 = 3
+
+// document.write("<br>result is " , result );
+
+// Q No. 3
+
+// var name = prompt("username");
+// alert("Hi! " + name + " welcome to our webpage");
+
+// Q No. 5
+// var number = prompt("please enter number" , 5);
+// if(number === number && !isNaN(number)){
+//     document.write("Multiplication table of &nbsp" + number + " <br><br><br><br>");
+
+//      document.write( number + " * 1" + " = " + number*1 , "<br>");
+//      document.write( number + " * 2" + " = " + number*2 , "<br>");
+//      document.write( number + " * 3" + " = " + number*3 , "<br>");
+//      document.write( number + " * 4" + " = " + number*4 , "<br>");
+//      document.write( number + " * 5" + " = " + number*5 , "<br>");
+//      document.write( number + " * 6" + " = " + number*6 , "<br>");
+//      document.write( number + " * 7" + " = " + number*7 , "<br>");
+//      document.write( number + " * 8" + " = " + number*8 , "<br>");
+//      document.write( number + " * 9" + " = " + number*9 , "<br>");
+//      document.write( number + " * 10" + " = " + number*10 , "<br>");
+// }
+// else{
+//     document.write(" You entered wrong number or non acceptable value please enter correct number or value ");
+// }
+
+//Q No. 7
+// var a = prompt("Enter 1st subject name");
+// var b = prompt("Enter 2nd subject name");
+// var c = prompt("Enter 3rd subject name");
+// var T_marks = 100; // per subject
+// var d = + prompt("Enter obtained marks of 1st subject");
+// var p1 = (d * 100) / T_marks;
+// var e = + prompt("Enter obtained marks of 1st subject");
+// var p2 = (e * 100) / T_marks;
+// var f = + prompt("Enter obtained marks of 1st subject");
+// var p3 = (f * 100) / T_marks;
+// var obtained_marks = d + e + f;
+// var total = 300;
+// var percentage = (obtained_marks * 100) / 300;
+// document.write("<b>Subject  Total marks  Obtained Marks  Percentage</b><br><br>");
+// document.write(a , "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" , T_marks , "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" , d , "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" , p1 , "%" , "<br>");
+// document.write(b , "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" , T_marks , "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" , e , "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" ,  p2 , "%" , "<br>");
+// document.write(c , "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" , T_marks , "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" , f , "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" , p3 , "%" , "<br>");
+// document.write("&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp"  , total , "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" , obtained_marks , "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" , percentage , "%");
+
+// ------Chapter No.  09 to 11---------
+
+//Q No. 1
+//    var city = prompt("please enter your city" );
+//    if(city === "karachi"){
+//    alert("welcome to city of light " +  city);
+//    }else{
+//        alert("you entered wrong value ");
+//    }
+
+//Q No. 2
+                
+//     var gender = prompt("please enter your gender" );
+//    if(gender === "male"){
+//    alert("Good Morning Sir. ");
+//    }else if(gender === "female"){
+//        alert("Good Morning Ma’am. ");
+//    }else{
+//        alert("you entered wrong value")
+//    }
+
+//Q No. 3
+// var trafficLight = prompt("please enter road traffic signal color" );
+//    if(trafficLight === "red"){
+//    alert("vehicles Must Stop. ");
+//    }else if(trafficLight === "yellow"){
+//        alert("Vehicles should get ready to move ");
+//    }else if(trafficLight === "green"){
+//        alert("vehicles can move now")
+//    }else{
+//        alert("you entered wrong road traffic signal color")
+//    }
